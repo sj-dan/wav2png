bbbc659d6546e22ef0aa3f1e977b181b4d64fa33
DanChan <sarodagenius@gmail.com>
2025-02-13 00:18:35 -0500

    Update build system

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-CURRENT_FOLDER=$PWD
-
-BUILD_FOLDER='./dist'
-
-rm -rf "$BUILD_FOLDER"
-mkdir -p "$BUILD_FOLDER"
-
-cd "$BUILD_FOLDER"
-cmake "$CURRENT_FOLDER"
-make -j$(nproc --all)
+cmake -S . -B dist -DCMAKE_BUILD_TYPE=Release
+cmake --build dist -j8
